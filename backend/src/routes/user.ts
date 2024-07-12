ea7783a64495f72517aa566580e664d4c7dfc011
Raghu Anand <raghuaanand@gmail.com>
2024-07-12 20:28:25 +0530

    backend completed

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,82 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from 'hono'
+import { sign } from 'hono/jwt'
+import { signinInput, signupInput} from '@raghuanand/verse-viber-zod'
+
+
+export const userRouter = new Hono<{
+    Bindings: {
+      DATABASE_URL: string
+      JWT_SECRET: string
+    }
+  }>()
+
+userRouter.post('/signup', async (c) => {
+
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate())
+  
+    const body = await c.req.json();
+    const { success } = signupInput.safeParse(body)
+    if(!success){
+      c.status(403)
+      return c.json({
+        message:"invalid inputs",
+      })
+    }
+  
+    try{
+      const user = await prisma.user.create({
+        data: {
+          email: body.email,
+          password: body.password,
+          name: body.name
+        }
+      });
+      const jwt = await sign({id: user.id}, c.env.JWT_SECRET);
+      return c.json({jwt});
+    } catch(e){
+      return  c.status(403)
+    }
+  
+  })
+  
+  userRouter.post('/signin', async (c) => {
+  
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate())
+  
+    const body = await c.req.json();
+    const { success } = signinInput.safeParse(body);
+  
+    if(!success){
+      c.status(411)
+      return c.json({
+        message: "invalid inputs"
+      })
+    }
+  
+    try{
+      const user = await prisma.user.findUnique({
+        where: {
+          email: body.email,
+          password: body.password
+        }
+      });
+  
+      if(!user){
+        c.status(404);
+        return c.json({
+          message: "user not found"
+        })
+      }
+  
+      const jwt = await sign({id: user.id}, c.env.JWT_SECRET);
+      return c.json({jwt})
+    }catch(e){
+      return c.status(403)
+    }
+  })
