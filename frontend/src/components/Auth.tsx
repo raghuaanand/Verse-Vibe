dde0d8c957c1c671edc1cef13e3aeface72cfe31
Raghu Anand <raghuaanand@gmail.com>
2024-07-31 11:32:44 +0530

    Update Auth.tsx

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -2,7 +2,7 @@ import { SignupType } from "@raghuanand/verse-viber-zod";
 import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { BACKEND_URL } from "../config";
+// import { BACKEND_URL } from "../config";
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
   const navigate = useNavigate();
