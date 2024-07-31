ca2253fb4a8456f910303be9d60605307abc5a94
Raghu Anand <raghuaanand@gmail.com>
2024-07-31 11:28:55 +0530

    Update index.ts

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -16,7 +16,7 @@ export const useBlog = ({ id }: { id: string }) => {
   const [blog, setBlog] = useState<Blog>();
   useEffect(() => {
     axios
-      .get(`${BACKEND_URL}/api/v1/blog/${id}`, {
+      .get(`https://backend.raghuaanand.workers.dev/api/v1/blog/${id}`, {
         headers: {
           Authorization: localStorage.getItem("token"),
         },
@@ -38,7 +38,7 @@ export const useBlogs = () => {
   const [blogs, setBlogs] = useState<Blog[]>([]);
   useEffect(() => {
     axios
-      .get(`${BACKEND_URL}/api/v1/blog/bulk`, {
+      .get(`https://backend.raghuaanand.workers.dev/api/v1/blog/bulk`, {
         headers: {
           Authorization: localStorage.getItem("token"),
         },
